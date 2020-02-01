c5f3e832c3f0d259a2c130acc80ca0a4b61c0169
matt.bmayer@gmail.com <kyle.causton@hotmail.com>
2020-01-31 23:21:51 -0500

    Basic player controller and prefab. More base folders.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private CapsuleCollider bodyCollider;
+    private Rigidbody bodyRB;
+    private CharacterController controller;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        bodyCollider = GetComponent<CapsuleCollider>();
+        bodyRB = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        float updown = Input.GetAxis("Vertical");
+        float leftright = Input.GetAxis("Horizontal");
 
-    
+        Vector3 movement = new Vector3(leftright, 0.0f, updown);
+
+        controller.Move(movement);
+    }
 }
